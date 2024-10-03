ec26ef4e0994f38ae1e4eeb18b5bcb365761e556
Kyle Roberts <push@krob.me>
2024-10-03 21:27:01 +0100

    AssetType

--- assets_api/Models/AssetType.cs
+++ assets_api/Models/AssetType.cs
@@ -3,11 +3,11 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace assets_api.Models;
 
-public class Manufacturer
+public class AssetType
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
-
-    [BsonElement("name")] public string? Name { get; set; }
+    
+    [BsonElement("Name")] public string? Name { get; set; }
 }
