e32660dc6338094f28806285eb2ff12da17e19b6
Kyle Roberts <push@krob.me>
2024-10-03 21:38:32 +0100

    Create Asset controller

---
+++ assets_api/Controllers/AssetController.cs
@@ -0,0 +1,30 @@
+﻿using assets_api.Models;
+using assets_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assets_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AssetController : ControllerBase
+{
+    private readonly AssetService _assetService;
+
+    public AssetController(AssetService assetService)
+    {
+        _assetService = assetService;
+    }
+    
+    [HttpGet]
+    public async Task<List<Asset>> GetAssets() =>
+        await _assetService.GetAssets();
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsset(Asset asset)
+    {
+        // TODO: Check valid variables e.g manufacturer
+        await _assetService.CreateAsset(asset);
+        
+        return CreatedAtAction(nameof(GetAssets), new { asset.Id }, asset);
+    }
+}
--- assets_api/Services/AssetService.cs
+++ assets_api/Services/AssetService.cs
@@ -7,6 +7,8 @@ namespace assets_api.Services;
 public class AssetService
 {
     private readonly IMongoCollection<Manufacturer> _manufacturersCollection;
+    private readonly IMongoCollection<AssetType> _assetTypesCollection;
+    private readonly IMongoCollection<Asset> _assetsCollection;
 
     public AssetService()
     {
@@ -14,10 +16,24 @@ public class AssetService
         var mongoDatabase = mongoClient.GetDatabase("Assets");
         
         _manufacturersCollection = mongoDatabase.GetCollection<Manufacturer>("Manufacturers");
+        _assetTypesCollection = mongoDatabase.GetCollection<AssetType>("AssetTypes");
+        _assetsCollection = mongoDatabase.GetCollection<Asset>("Assets");
     }
+    
+    public async Task<List<Asset>> GetAssets() =>
+        await _assetsCollection.Find(_ => true).ToListAsync();
+    
+    public async Task<List<AssetType>> GetAssetTypes() =>
+        await _assetTypesCollection.Find(_ => true).ToListAsync();
     
     public async Task<List<Manufacturer>> GetManufacturers() =>
         await _manufacturersCollection.Find(m => true).ToListAsync();
+    
+    public async Task CreateAsset(Asset asset) =>
+        await _assetsCollection.InsertOneAsync(asset);
+    
+    public async Task CreateAssetType(AssetType assetType) =>
+        await _assetTypesCollection.InsertOneAsync(assetType);
     
     public async Task CreateManufacturer(Manufacturer manufacturer) =>
         await _manufacturersCollection.InsertOneAsync(manufacturer);
