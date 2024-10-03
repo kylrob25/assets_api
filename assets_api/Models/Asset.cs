d7061894daaeb480e4255b71922a738e77da137a
Kyle Roberts <push@krob.me>
2024-10-03 21:30:10 +0100

    Start creating asset schema

---
+++ assets_api/Models/Asset.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace assets_api.Models;
+
+public class Asset
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    
+    [BsonElement("name")] public string? Name { get; set; }
+    
+    [BsonElement("type")] public string? Type { get; set; }
+    
+    [BsonElement("manufacturer")] public string? Manufacturer { get; set; }
+    
+    [BsonElement("quantity")] public int? Quantity { get; set; }
+}
